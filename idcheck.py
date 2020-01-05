f3e7b7a0b6b66d7c63d91cf88ef0698f5cf3f5a9
Roie <Roie-@users.noreply.github.com>
2020-01-05 04:29:33 +0200

    update

--- idcheck.py
+++ idcheck.py
@@ -1,4 +1,4 @@
-import math  # for counting digits of integer type
+import math
 
 class IDIterator:
     def __init__(self,id):
@@ -14,36 +14,67 @@ class IDIterator:
         return "{}".format(self._id)
 
 
-# --------------------------------------------------------------
-# valid ID check function: return True if valid, False otherwise
-# --------------------------------------------------------------
+
 def check_id_valid(id_number):
-    ''' variable id_number: ID number
-        variable _digits: number of digits/characters
     '''
-    # count number of digits (string or integer)
-    if (isinstance(id_number,int)) and id_number > 0:
-        _digits = int(math.log10(id_number))+1
-    elif (isinstance(id_number,str)) and id_number.isdigit():
-        _digits = len(str(id_number))
-    else:
-        raise ValueError("check_id_valid expects only 9 positive digits.")
+    Take the ID number on id_number and check
+    if it's a valid ID number.
+    return True if valid, False otherwise
 
-    # valid input check: 9 digits'''
-    if _digits != 9:
-        raise ValueError("Please enter exectly 9 positive digits.")
+    variables:
+    id_number --> input from user (string/integer)
+    _digits ----> number of digits/characters count
 
-    '''one liner valid ID check'''
+    raises:
+    ValueError --> Not an only numbers string or negetive integer
+
+    returns:
+    Booliane --> True/False   
+    '''
+
+    # count number of digits (string or integer)    
+    # Using log10 method for performance gains
+    # when counting digits of integer type.
+    # if (isinstance(id_number,int)) and id_number > 0:
+    #     _digits = int(math.log10(id_number))+1
+    # elif (isinstance(id_number,str)) and id_number.isdigit():
+    #     _digits = len(str(id_number))
+    # else:
+    #     raise ValueError("check_id_valid expects only 9 positive digits.")
+
+    
+    # if _digits != 9:
+    #     raise ValueError("Please enter exectly 9 positive digits.")
+
+    # one liner valid ID check
     if sum(sum(map(int, str(int(a)*(i%2+1)))) for i, a in enumerate("{0:09d}".format(int(id_number)))) % 10 == 0:
         return True
     else:
         return False
 
 
+# --------------------
+# input check function
+# --------------------
+def check_input(id_number):
+    # count number of digits (string or integer)
+    # Using log10 method for performance gains
+    # when counting digits of integer type.
+    if (isinstance(id_number,int)) and id_number > 0:
+        _digits = int(math.log10(id_number))+1
+    elif (isinstance(id_number,str)) and id_number.isdigit():
+        _digits = len(str(id_number))
+    else:
+        _digits = 0
+
+    return _digits
+
+
 # ------------------
 # generator function
 # ------------------
 def id_generator(id):
+    '''variable id: ID number'''
     return id
 
 
@@ -51,11 +82,34 @@ def id_generator(id):
 # start of program
 # ----------------
 def main():
-    # id_number = input("Please enter ID number(9 digits only):\n")
-    id_number = '434266699'
+    # while True:
+        # id_number = '03716972'
+        # try:
+    id_number = input("Please enter ID number(9 digits only):\n")            
+        # except:
+            # print('Please enter 9 numeric digits.')
+            # continue
+        # break
+
+
+    # if _digits !=9:
+        # print('Please enter a 9 positive number.')
+           
+
+
+
+    # id_number = 434266699
+
+    if check_input(id_number) != 9:
+        raise ValueError("Please enter exectly 9 positive digits.")
+
+    print(check_id_valid(id_number))
+    
     iter = IDIterator(123456780)
     print(iter)
-    print(check_id_valid(id_number))
-    print(id_generator(123456780))
+    
+    print(id_generator.__doc__)
+    print(check_id_valid.__doc__)
+    print(math.__doc__)
 
 main()
