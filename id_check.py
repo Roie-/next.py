f9869f7e7abe00e4fde02ad123c30f879e5c0ae5
Roie <Roie-@users.noreply.github.com>
2020-01-18 16:18:35 +0200

    fix

--- id_check.py
+++ id_check.py
@@ -1,29 +1,23 @@
 import math
 
-# ---------------
-# Class Exception
-# ---------------
+
+
+### Class Exception ###
 class NotPositiveNumber(Exception):
     def __init__(self,arg):
         self._arg = arg
 
-
     def __str__(self):
         raise "argument {} is not a positive integer.".format(self._arg)    
-
 
     def get_arg(self):
         return self._arg
 
 
 
-
-# --------------
-# Class Iterator
-# --------------
+#### Class Iterator ###
 class IDIterator:
-    '''
-    Custom iterators
+''' Summery: Custom iterators
 
     Params:
     self._id ----> Input ID number from user
@@ -31,41 +25,31 @@ class IDIterator:
 
     Raises:
     StopIteration --> When ID number higher from limit '''
-
     def __init__(self,id=0,stop=999999999):
         self._id = id
         self._stop = stop
 
-
     def __iter__(self):
         return self
-
 
     def __next__(self):
         if self._id >= self._stop:
             raise StopIteration()
-
         self._id += 1
         return self._id
-
 
     def __str__(self):
         return "{}".format(self._id)
 
 
 
-
-# ------------------
-# Generator function 
-# ------------------
+#### Generator function ###
 def id_generator(id):
-    '''
-    Summery: Generate next ID number
+''' Summery: Generate next ID number
 
     Params: 
-        id ----> Input ID number
-        stop --> ID number limit'''
-
+    id ----> Input ID number
+    stop --> ID number limit'''
     id += 1
     stop = 999999999
     while id <= stop:
@@ -74,19 +58,13 @@ def id_generator(id):
 
 
 
-
-# --------------------
-# Function Input check
-# --------------------
+### Function Input check ###
 def input_check(id):
-    '''
-    Summery: Raise exception if not valid input
-            (string/negetive num/length != 9)
-    
+''' Summery: Raise exception if not valid input
+            (string/negetive num/length != 9) 
     Raises:
         NotPositiveNumber --> negetive number or string
         ValueError ---------> Not exectly 9 positive digits'''
-
     try:            
         if not (isinstance(id,int)) or id < 0:
             raise NotPositiveNumber(id)
@@ -94,25 +72,18 @@ def input_check(id):
             print('Function Expected positive integer, and instead got {} {}.'.format(id, type(e.get_arg())))
     else:
         digits = int(math.log10(id))+1
-
-    
+ 
     if digits != 9:
         raise ValueError("Please enter exectly 9 positive integer.")
    
 
 
-
-# --------------------
-# ID number validation
-# --------------------
+### ID number validation ###
 def check_id_valid(id_number):
-    '''
-    Summery: Chek if ID number is valid
+''' Summery: Chek if ID number is valid
              return True if yes, False otherwise
-
     Params:
         id_number --> Input from user'''
-
     if sum(sum(map(int, str(int(a)*(i%2+1)))) for i, a in enumerate("{0:09d}".format(int(id_number)))) % 10 == 0:
         return True
     else:
@@ -120,25 +91,21 @@ def check_id_valid(id_number):
 
 
 
-
-# ----------------
-# Start of program
-# ----------------
-    '''
-    process:
+### Start of program ###
+''' Process:
     1) Get and check input from user
     2) Print 10 valid ID number from Iterator
     3) Print 10 valid ID number from Generator
 
     params:
-        id_number --> Input from user
-        id_valid ---> Booliane Result from function
-                      check_id_valid (True/False)
-        id_count ---> Number of valid ID's
-        id_iter ----> Object of type Iterator
-        id_gen -----> Object of type Generator'''
-
+    id_number --> Input from user
+    id_valid ---> Booliane Result from function
+                  check_id_valid (True/False)
+    id_count ---> Number of valid ID's
+    id_iter ----> Object of type Iterator
+    id_gen -----> Object of type Generator'''
 if __name__ == '__main__':
+    
     id_number = int(input("Please enter ID number (9 integer digits only):\n"))
     input_check(id_number)          
     id_valid = check_id_valid(id_number)
@@ -163,7 +130,6 @@ if __name__ == '__main__':
             id_number = next(id_iter)
             continue    
    
-
     print('\n10 valid ID from Generator\n--------------------------')
     id_count = 0
     id_gen = id_generator(id_number)
